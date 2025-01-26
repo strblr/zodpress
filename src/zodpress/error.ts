78ff9b47a178a8093490659f377fcfb194549ca9
strblr <30187797+strblr@users.noreply.github.com>
2025-01-26 20:33:38 +0100

    refactor: use Error.prototype.name

--- src/zodpress/error.ts
+++ src/zodpress/error.ts
@@ -2,14 +2,14 @@ import type { ZodIssue } from "zod";
 import { isEmpty } from "./utils";
 
 export class ValidationError extends Error {
-  type = "validation_error";
   headersErrors?: ZodIssue[];
   paramsErrors?: ZodIssue[];
   queryErrors?: ZodIssue[];
   bodyErrors?: ZodIssue[];
 
   constructor() {
-    super("Zodpress validation error");
+    super();
+    this.name = "ValidationError";
   }
 
   isEmpty() {
